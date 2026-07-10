767497de61754841af4da69ef9d3f3c3cf028e44
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-07-10 04:13:44 +0100

    Replace Stack Navigator with React Router

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -1,23 +1,30 @@
 // src/navigation/AppNavigator.js
 import React from "react";
-import TabNavigator from "./TabNavigator";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Screens
 import AuthLoadingScreen from "../screens/AuthLoadingScreen";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
+import ForumScreen from "../screens/ForumScreen";
+import HomeScreen from "../screens/HomeScreen";
 import PropertyDetailScreen from "../screens/PropertyDetailScreen";
-
-const Stack = createStackNavigator();
+import SearchScreen from "../screens/SearchScreen";
+import UploadScreen from "../screens/UploadScreen";
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="AuthLoading" component={AuthLoadingScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="AppNavigator" component={TabNavigator} />
-        <Stack.Screen name="PropertyDetail" component={PropertyDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/auth" element={<AuthLoadingScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/forum" element={<ForumScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/upload" element={<UploadScreen />} />
+        <Route path="/property/:id" element={<PropertyDetailScreen />} />
+      </Routes>
+    </Router>
   );
 }
