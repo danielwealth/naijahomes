fcd5d781fa2014037f9a8ce719912412ba825820
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-07-10 01:10:16 +0100

    Implement routing in App component
    Refactored App component to use React Router for navigation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-// src/App.js
 import React from "react";
-import AppNavigator from "./navigation/AppNavigator";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Properties from "./pages/Properties";
+import Contact from "./pages/Contact";
 
-export default function App() {
-  return <AppNavigator />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
