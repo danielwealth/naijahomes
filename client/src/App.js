eefef4781b0c51e95a03efe52db16798715a3369
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-07-10 01:45:26 +0100

    Implement routing for application screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 // client/src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import screens
+import AuthLoading from "./screens/AuthLoading";
+import Forum from "./screens/Forum";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import PropertyDetail from "./screens/PropertyDetail";
+import Register from "./screens/Register";
+import Search from "./screens/Search";
+import Upload from "./screens/Upload";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Naijahomes</h1>
-      <p>Your property search starts here.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthLoading />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/property/:id" element={<PropertyDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
